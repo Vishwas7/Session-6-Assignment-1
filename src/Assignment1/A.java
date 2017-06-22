c445dd140bc8195b6bd9547b2ab9d5be36d7e252
Vishwas7 <svishwas788@gmail.com>
2017-06-22 14:59:56 +0530

    Session 6 Asignment -1

---
+++ src/Assignment1/A.java
@@ -0,0 +1,39 @@
+package Assignment1;
+
+
+/**
+ * -In this program I'll create four classes in a single program
+ *  named as class A, class B, class C, class D
+ *  and each class having main method and each class will print something.
+ *  We con't create more than one public class in a program.
+ */
+
+public class A {  //  First Class, which is public class.
+	
+
+	public static void main(String[] args) {  // main method
+		
+		
+		System.out.println("Hello Clas A"); // console window
+	}
+
+}
+class B { // Second class.
+	
+	public static void main(String[]args){ //main method
+		
+		System.out.println("Hello Class B");// console window
+	}
+}
+class C { // Third class
+	public static void main(String[]args){ //main method
+		
+		System.out.println("Hello Class C"); // console window
+	}
+}
+class D { // Fourth Class
+	public static void main(String[]args){ //main method
+		
+		System.out.println("Hello Class D"); //// console window
+	}
+}
